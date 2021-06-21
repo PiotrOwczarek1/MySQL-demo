bf3ac783d4678a8ab0f1ad54695b6b93ebad5225
PiotrOwczarek1 <PiotrOwczarek1@users.noreply.github.com>
2021-06-21 09:18:35 +0200

    branch4

--- src/main/java/com/example/accessingdatamysql/demo/AccessingDataMysqlApplication.java
+++ src/main/java/com/example/accessingdatamysql/demo/AccessingDataMysqlApplication.java
@@ -10,7 +10,7 @@ public class AccessingDataMysqlApplication {
   public static void main(String[] args) {
     SpringApplication.run(AccessingDataMysqlApplication.class, args);
     System.out.println("master");
-    System.out.println("branch1");
+    System.out.println("branch4");
     System.out.println("po merge");
     System.out.println("po merge2");
 	System.out.println("laczony merge");
